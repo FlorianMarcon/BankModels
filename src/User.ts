410ce0189e4e86eba2aa0b924088de61d7cd26e1
florian.marcon@epitech.eu <florian.marcon@epitech.eu@epitech.eu>
2020-04-12 18:39:25 +0200

    User Create:  - Create file UserRequests  - (UserRequest) Create interface IMeArgs

--- src/User.ts
+++ src/User.ts
@@ -1,6 +1,15 @@
+
+export enum Roles {
+	ADMIN = 'ADMIN',
+	TUTOR = 'TUTOR',
+	TUTELLES = 'TUTELLES',
+}
+
 export interface User {
 	_id			:		string;
 
 	firstname	:		string;
 	lastname	:		string;
+
+	role		:	Roles;
 }
