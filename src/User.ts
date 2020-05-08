3ebd919b88f13ea4f1f058a289eb54082672cda7
flonflon <florian.marcon@epitech.eu>
2020-05-08 11:36:46 +0200

    (User) Create IAuth, IUserDelete, IUserUpdate, IUserCreate and complete User interface

--- src/User.ts
+++ src/User.ts
@@ -3,13 +3,52 @@ export enum Roles {
 	ADMIN = 'ADMIN',
 	TUTOR = 'TUTOR',
 	TUTELLES = 'TUTELLES',
+	LAMBDA	=	'LAMBDA'
 }
 
 export interface User {
 	_id			:		string;
 
 	firstname	:		string;
+
 	lastname	:		string;
 
+	email		:		string;
+
+	password	:		string;
+
 	role		:	Roles;
+}
+
+export interface IUserCreate {
+	firstname	:		string;
+
+	lastname	:		string;
+
+	email		:		string;
+
+	password	:		string;
+}
+
+export interface IUserUpdate {
+	_id			:		string;
+
+	firstname?	:		string;
+
+	lastname?	:		string;
+
+	email?		:		string;
+
+	password?	:		string;
+}
+
+export interface IUserDelete {
+	_id			:		string;
+}
+
+export interface IAuth {
+
+	email		:		string;
+
+	password	:		string;
 }
