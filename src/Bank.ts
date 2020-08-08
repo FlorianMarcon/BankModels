26be178936bcc62b32a4c7454662083172b8e1ad
FlorianMarcon <florian.marcon@epitech.eu>
2020-08-08 10:28:12 +0200

    Create Bank interfaces

---
+++ src/Bank.ts
@@ -0,0 +1,77 @@
+import { IAddress } from "./Address";
+
+export enum BankType {
+    CREDIT_AGRICOLE
+}
+
+export interface IBank {
+
+    /**
+     * Id of the bank
+     */
+    _id:   string;
+
+    /**
+     * Type de la banque
+     */
+    type:   BankType;
+
+    /**
+     * Agence
+     */
+    agency:     String;
+
+    /**
+     * Addresse de la banque
+     */
+    address:    IAddress;
+
+    /**
+     * Boolean pour savoir si cette Bank est archivé ou non
+     */
+    isArchivated: boolean;
+}
+
+export interface IBankCreate {
+    /**
+     * Type de la banque
+     */
+    type:   BankType;
+
+    /**
+     * Agence
+     */
+    agency:     String;
+
+    /**
+     * Addresse de la banque
+     */
+    address:    IAddress;
+}
+
+export interface IBankUpdate {
+    /**
+     * Id of the bank
+     */
+    _id:   string;
+    
+    /**
+     * Type de la banque
+     */
+    type?:   BankType;
+
+    /**
+     * Agence
+     */
+    agency?:     String;
+
+    /**
+     * Addresse de la banque
+     */
+    address?:    IAddress;
+
+    /**
+     * Boolean pour savoir si cette Bank est archivé ou non
+     */
+    isArchivated?:   boolean;
+}
