3c27180014508bf04d0a664e56e6149678983768
FlorianMarcon <florian.marcon@epitech.eu>
2020-10-06 12:18:54 +0100

    (Budget) Create field description

--- src/Budget.ts
+++ src/Budget.ts
@@ -1,18 +1,39 @@
-
+/**
+ * Object reprensenting Budget
+ */
 export interface IBudget {
+	/**
+	 * Id of the budget
+	 */
 	_id			:		string;
 
+	/**
+	 * Name of the budget
+	 */
 	name		:		string;
 
+	/**
+	 * Maximum amount to spend in this budget
+	 */
 	amount		:		number; /* in cts*/
 
+	/**
+	 * Boolean to say if budget is archivated or not
+	 */
 	archivated	:		boolean;
+
+	/**
+	 * Description of the budget
+	 */
+	description	:		string | null;
 }
 
 export interface IBudgetCreate {
 	name		:		string;
 
 	amount?		:		number; /* in cts*/
+
+	description?	:		string | null;
 }
 
 export interface IBudgetsOptions {
@@ -29,4 +50,6 @@ export interface IBudgetUpdate {
 	amount?		:		number; /* in cts*/
 
 	archivated?	:		boolean;
+
+	description?	:		string | null;
 }
